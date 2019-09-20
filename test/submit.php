d6bdbabe5e616b13924013176b98064ee6b8c8d1
Giancarlo <giancarlo.birello@gmail.com>
2019-09-20 10:51:11 +0200

    Add queue test

--- test/submit.php
+++ test/submit.php
@@ -8,13 +8,13 @@ use Drupal\Core\State\State;
 //after launched the submit script exit and mailLoop script run in background
 //to see mailLoop output check /tmp/runners.log file
 //i.e. tail -f /tmp/runners.log
+//Launch submit when mainLoop is running produce adding elements to queue
 
 //build command
 $drush_path = "/var/www/archipelago/vendor/drush/drush/";
 $mainLoop_path = "/var/www/archipelago/web/modules/contrib/strawberry_runners/src/Scripts";
 $cmd = $drush_path . 'drush scr mainLoop --script-path=' . $mainLoop_path;
 $outputfile = "/tmp/runners.log";
-
 
 //check state
 $data = \Drupal::state()->get('strawberryfield_mainLoop');
@@ -26,13 +26,34 @@ list($processId, $lastRunTime) = $data;
 $delta = \Drupal::time()->getCurrentTime() - $lastRunTime;
 
 if ($delta < 10) {
-	echo 'mainLoop running' . PHP_EOL;
+  echo 'mainLoop running' . PHP_EOL;
+
+  //add elements to queue
+  $queue = \Drupal::queue('strawberry_runners');
+  for ($x = 1; $x <= 3; $x++) {
+    $element = "Element " . $x;
+    $queue->createItem($element);
+  }
+  $totalItems = $queue->numberOfItems();
+  echo 'Queue totalItems ' . $totalItems . PHP_EOL;
+
 }
 else {
-	echo 'mainLoop to start' . PHP_EOL;
+  echo 'mainLoop to start' . PHP_EOL;
+
+  //clear and populate queue
+  $queue = \Drupal::queue('strawberry_runners');
+  $queue->deleteQueue();
+  for ($x = 1; $x <= 5; $x++) {
+    $element = "Element " . $x;
+    $queue->createItem($element);
+  }
+  $totalItems = $queue->numberOfItems();
+  echo 'Queue totalItems ' . $totalItems . PHP_EOL;
+
   $pid = shell_exec(sprintf("%s > %s 2>&1 & echo $!", $cmd, $outputfile));
   $data = [
-		'processId' => $pid,
+    'processId' => $pid,
     'lastRunTime' => \Drupal::time()->getCurrentTime(),
   ];
   \Drupal::state()->set('strawberryfield_mainLoop', implode(',', $data));
