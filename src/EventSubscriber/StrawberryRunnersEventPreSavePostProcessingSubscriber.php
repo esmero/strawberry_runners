1cf8cee33fe9625424ab7fb53bf330e2c249ad85
Diego Pino Navarro <dpino@metro.org>
2020-04-08 15:36:25 -0400

    Small fixes. So much more to do yet
    @giancarlobi not sure why the hook update N is/was not running, i also changed the number because it really ignored it . Maybe its the lack of proper versioning of this module yet?
    So do have it running i did this
    
    drush eval "module_load_install('strawberry_runners'); strawberry_runners_update_8100();"

--- src/EventSubscriber/StrawberryRunnersEventPreSavePostProcessingSubscriber.php
+++ src/EventSubscriber/StrawberryRunnersEventPreSavePostProcessingSubscriber.php
@@ -3,11 +3,10 @@
 namespace Drupal\strawberry_runners\EventSubscriber;
 
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\strawberryfield\Event\StrawberryfieldServiceEvent;
-use Drupal\strawberryfield\EventSubscriber\StrawberryfieldEventJsonProcessingSubscriber;
+use Drupal\strawberryfield\Event\StrawberryfieldCrudEvent;
+use Drupal\strawberryfield\EventSubscriber\StrawberryfieldEventPresaveSubscriber;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\Core\StringTranslation\TranslationInterface;
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Logger\LoggerChannelFactoryInterface;
@@ -21,15 +20,18 @@ use Drupal\strawberry_runners\Plugin\StrawberryRunnersPostProcessorPluginManager
 /**
  * Event subscriber for SBF bearing entity json process event.
  */
-class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEventJsonProcessingSubscriber  {
+class StrawberryRunnersEventPreSavePostProcessingSubscriber extends StrawberryfieldEventPresaveSubscriber {
 
 
   use StringTranslationTrait;
 
   /**
+   *
+   * Run as late as possible.
+   *
    * @var int
    */
-  protected static $priority = -700;
+  protected static $priority = -2000;
 
   /**
    * The messenger.
@@ -98,12 +100,10 @@ class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEven
   protected $strawberryRunnerProcessorPluginManager;
 
   /**
-   * StrawberryRunnersEventJsonProcessingSubscriber constructor.
+   * StrawberryRunnersEventPreSavePostProcessingSubscriber constructor.
    *
-   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
    * @param \Drupal\Core\StringTranslation\TranslationInterface $string_translation
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
-   * @param \Symfony\Component\Serializer\SerializerInterface $serializer
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
    * @param \Drupal\Core\StreamWrapper\StreamWrapperManagerInterface $stream_wrapper_manager
@@ -111,34 +111,35 @@ class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEven
    * @param \Drupal\strawberry_runners\Plugin\StrawberryRunnersPostProcessorPluginManager $strawberry_runner_processor_plugin_manager
    */
   public function __construct(
-    EntityTypeManagerInterface $entity_type_manager,
-    EntityStorageInterface $storage,
     TranslationInterface $string_translation,
     MessengerInterface $messenger,
-    ConfigFactoryInterface $config_factory,
     LoggerChannelFactoryInterface $logger_factory,
+    ConfigFactoryInterface $config_factory,
     StreamWrapperManagerInterface $stream_wrapper_manager,
     FileSystemInterface $file_system,
+    EntityTypeManagerInterface $entity_type_manager,
     StrawberryRunnersPostProcessorPluginManager $strawberry_runner_processor_plugin_manager
   ) {
     $this->stringTranslation = $string_translation;
     $this->messenger = $messenger;
     $this->loggerFactory = $logger_factory;
     $this->configFactory = $config_factory;
-    $this->storage = $storage;
-    $this->entityTypeManager = $entity_type_manager;
     $this->streamWrapperManager = $stream_wrapper_manager;
     $this->fileSystem = $file_system;
+    $this->entityTypeManager = $entity_type_manager;
     $this->strawberryRunnerProcessorPluginManager = $strawberry_runner_processor_plugin_manager;
   }
 
   /**
-   * Method called when Event occurs.
+   *  Method called when Event occurs.
    *
-   * @param \Drupal\strawberryfield\Event\StrawberryfieldServiceEvent $event
-   *   The event.
+   * @param \Drupal\strawberryfield\Event\StrawberryfieldCrudEvent $event
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function onJsonInvokeProcess(StrawberryfieldServiceEvent $event) {
+  public function onEntityPresave(StrawberryfieldCrudEvent $event) {
 
     /* @var $plugin_config_entities \Drupal\strawberry_runners\Entity\strawberryRunnerPostprocessorEntity[] */
     $plugin_config_entities = $this->entityTypeManager->getListBuilder('strawberry_runners_postprocessor')->load();
@@ -189,8 +190,9 @@ class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEven
                 );
                 /** @var $file FileInterface; */
                 if ($file) {
-                  $this->add_file_usage($file, $entity->id(), $entity_type_id);
-                  $updated++;
+                  //$this->add_file_usage($file, $entity->id(), $entity_type_id);
+                  //$updated++;
+                  $this->processFile($file);
                 }
                 else {
                   $this->messenger()->addError(
@@ -206,6 +208,10 @@ class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEven
         }
       }
     }
+    $current_class = get_called_class();
+    $event->setProcessedBy($current_class, TRUE);
+    $this->messenger->addStatus(t('Post processor was invoked'));
+
   }
 
   /**
@@ -230,7 +236,7 @@ class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEven
       // Local stream.
       $cache_key = md5($uri);
       if (empty($this->instanceCopiesOfFiles[$cache_key])) {
-        if (!($this->instanceCopiesOfFiles[$cache_key] = $this->fileSystem->copy($uri, 'temporary://sbr_' . $cache_key . '_' . basename($uri), FileSystemInterface::FILE_EXISTS_REPLACE))) {
+        if (!($this->instanceCopiesOfFiles[$cache_key] = $this->fileSystem->copy($uri, 'temporary://sbr_' . $cache_key . '_' . basename($uri), FileSystemInterface::EXISTS_REPLACE))) {
           $this->loggerFactory->get('strawberry_runners')
             ->notice('Unable to create local temporary copy of remote file for Strawberry Runners Post processing File %file.',
               [
@@ -267,7 +273,9 @@ class StrawberryRunnersEventJsonProcessingSubscriber extends StrawberryfieldEven
   public function __destruct() {
     // Get rid of temporary files created for this instance.
     foreach ($this->instanceCopiesOfFiles as $uri) {
-      \Drupal::service('file_system')->unlink($uri);
+      error_log('should destroy '.$uri);
+      error_log('better to keep usage count?');
+      // \Drupal::service('file_system')->unlink($uri);
     }
   }
 
