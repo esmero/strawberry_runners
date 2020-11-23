391101bf29f89f4ac356256245773e1c25c55543
Diego Pino Navarro <dpino@metro.org>
2020-11-23 11:23:38 -0500

    OK. I got XPath is working
    It was a namespace (ns)
    @giancarlobi ready to test

--- src/Plugin/StrawberryRunnersPostProcessor/OcrPostProcessor.php
+++ src/Plugin/StrawberryRunnersPostProcessor/OcrPostProcessor.php
@@ -34,15 +34,15 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
    */
   public function defaultConfiguration() {
     return [
-      'source_type' => 'asstructure',
-      'mime_type' => ['application/pdf'],
-      'path' => '',
-      'path_tesseract' => '',
-      'arguments' => '',
-      'arguments_tesseract' => '',
-      'output_type' => 'json',
-      'output_destination' => 'subkey',
-    ] + parent::defaultConfiguration();
+        'source_type' => 'asstructure',
+        'mime_type' => ['application/pdf'],
+        'path' => '',
+        'path_tesseract' => '',
+        'arguments' => '',
+        'arguments_tesseract' => '',
+        'output_type' => 'json',
+        'output_destination' => 'subkey',
+      ] + parent::defaultConfiguration();
   }
 
 
@@ -82,14 +82,14 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
         'as:audio' => 'as:audio',
         'as:video' => 'as:video',
         'as:text' => 'as:text',
-        'as:application' =>  'as:application',
+        'as:application' => 'as:application',
       ],
       '#default_value' => (!empty($this->getConfiguration()['jsonkey']) && is_array($this->getConfiguration()['jsonkey'])) ? $this->getConfiguration()['jsonkey'] : [],
       '#states' => [
         'visible' => [
           ':input[name="pluginconfig[source_type]"]' => ['value' => 'asstructure'],
-          ],
         ],
+      ],
       '#required' => TRUE,
     ];
 
@@ -108,11 +108,11 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
     ];
 
     $element['arguments'] = [
-       '#type' => 'textfield',
-       '#title' => $this->t('Any additional argument your executable binary requires.'),
-       '#default_value' => !empty($this->getConfiguration()['arguments']) ? $this->getConfiguration()['arguments'] : '%file',
-       '#description' => t('Any arguments your binary requires to run. Use %file as replacement for the file if the executable requires the filename to be passed under a specific argument.'),
-       '#required' => TRUE,
+      '#type' => 'textfield',
+      '#title' => $this->t('Any additional argument your executable binary requires.'),
+      '#default_value' => !empty($this->getConfiguration()['arguments']) ? $this->getConfiguration()['arguments'] : '%file',
+      '#description' => t('Any arguments your binary requires to run. Use %file as replacement for the file if the executable requires the filename to be passed under a specific argument.'),
+      '#required' => TRUE,
     ];
 
 
@@ -151,20 +151,20 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
         'ownkey' => 'In the same Source Metadata but inside its own, top level, "as:flavour" subkey based on the given machine name of the current plugin',
         'plugin' => 'As Input for another processor Plugin',
       ],
-      '#default_value' => (!empty($this->getConfiguration()['output_destination']) && is_array($this->getConfiguration()['output_destination']))? $this->getConfiguration()['output_destination']: [],
+      '#default_value' => (!empty($this->getConfiguration()['output_destination']) && is_array($this->getConfiguration()['output_destination'])) ? $this->getConfiguration()['output_destination'] : [],
       '#description' => t('As Input for another processor Plugin will only have an effect if another Processor is setup to consume this ouput.'),
       '#required' => TRUE,
     ];
 
-     $element['timeout'] = [
-       '#type' => 'number',
-       '#title' => $this->t('Timeout in seconds for this process.'),
-       '#default_value' => $this->getConfiguration()['timeout'],
-       '#description' => $this->t('If the process runs out of time it can still be processed again.'),
-       '#size' => 2,
-       '#maxlength' => 2,
-       '#min' => 1,
-     ];
+    $element['timeout'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Timeout in seconds for this process.'),
+      '#default_value' => $this->getConfiguration()['timeout'],
+      '#description' => $this->t('If the process runs out of time it can still be processed again.'),
+      '#size' => 2,
+      '#maxlength' => 2,
+      '#min' => 1,
+    ];
     $element['weight'] = [
       '#type' => 'number',
       '#title' => $this->t('Order or execution in the global chain.'),
@@ -173,7 +173,6 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
 
     return $element;
   }
-
 
 
   public function onDependencyRemoval(array $dependencies) {
@@ -199,8 +198,8 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
     // In this case it will contain an absolute Path to a File.
     // Needed since this executes locally on the server via SHELL.
 
-    $input_property =  $this->pluginDefinition['input_property'];
-    $input_argument =  $this->pluginDefinition['input_argument'];
+    $input_property = $this->pluginDefinition['input_property'];
+    $input_argument = $this->pluginDefinition['input_argument'];
     $file_uuid = isset($io->input->metadata['dr:uuid']) ? $io->input->metadata['dr:uuid'] : NULL;
     $node_uuid = isset($io->input->nuuid) ? $io->input->nuuid : NULL;
     $config = $this->getConfiguration();
@@ -210,7 +209,7 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
     if (isset($io->input->{$input_property}) && $file_uuid && $node_uuid) {
       // To be used by miniOCR as id in the form of {nodeuuid}/canvas/{fileuuid}/p{pagenumber}
       $page_number = isset($io->input->{$input_argument}) ? (int) $io->input->{$input_argument} : 1;
-      $pageid = $node_uuid.'/canvas/'.$file_uuid.'/p'.$page_number;
+      $pageid = $node_uuid . '/canvas/' . $file_uuid . '/p' . $page_number;
       setlocale(LC_CTYPE, 'en_US.UTF-8');
       $execstring = $this->buildExecutableCommand($io);
       error_log($execstring);
@@ -227,9 +226,10 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
 
         $miniocr = $this->hOCRtoMiniOCR($output, $pageid);
         error_log($miniocr);
-        $io->output =  $miniocr;
+        $io->output = $miniocr;
       }
-    } else {
+    }
+    else {
       \throwException(new \InvalidArgumentException);
     }
   }
@@ -246,8 +246,8 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
    * @return null|string
    */
   public function buildExecutableCommand(\stdClass $io) {
-    $input_property =  $this->pluginDefinition['input_property'];
-    $input_argument =  $this->pluginDefinition['input_argument'];
+    $input_property = $this->pluginDefinition['input_property'];
+    $input_argument = $this->pluginDefinition['input_argument'];
     // Sets the default page to 1 if not passed.
     $file_path = isset($io->input->{$input_property}) ? $io->input->{$input_property} : NULL;
     $page_number = isset($io->input->{$input_argument}) ? (int) $io->input->{$input_argument} : 1;
@@ -266,35 +266,40 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
     // First gs -dBATCH -dNOPAUSE -sDEVICE=pnggray -r300 -dUseCropBox -sOutputFile=somepage_pagenumber.png %file
 
     $command = '';
-    $can_run_gs = \Drupal::service('strawberryfield.utility')->verifyCommand($execpath_gs);
-    $can_run_tesseract = \Drupal::service('strawberryfield.utility')->verifyCommand($execpath_tesseract);
+    $can_run_gs = \Drupal::service('strawberryfield.utility')
+      ->verifyCommand($execpath_gs);
+    $can_run_tesseract = \Drupal::service('strawberryfield.utility')
+      ->verifyCommand($execpath_tesseract);
     $filename = pathinfo($file_path, PATHINFO_FILENAME);
-    $sourcefolder =  pathinfo($file_path,PATHINFO_DIRNAME);
-    $sourcefolder = strlen($sourcefolder)> 0 ? $sourcefolder.'/' : sys_get_temp_dir().'/';
+    $sourcefolder = pathinfo($file_path, PATHINFO_DIRNAME);
+    $sourcefolder = strlen($sourcefolder) > 0 ? $sourcefolder . '/' : sys_get_temp_dir() . '/';
     $gs_destination_filename = "{$sourcefolder}{$filename}_{$page_number}.png";
     if ($can_run_gs &&
       $can_run_tesseract &&
-      (strpos($arguments_gs, '%file' ) !== FALSE) &&
-      (strpos($arguments_tesseract, '%file' ) !== FALSE)) {
+      (strpos($arguments_gs, '%file') !== FALSE) &&
+      (strpos($arguments_tesseract, '%file') !== FALSE)) {
       $arguments_gs = "-dBATCH -dNOPAUSE -r300 -dUseCropBox -dQUIET -sDEVICE=pnggray -dFirstPage={$page_number} -dLastPage={$page_number} -sOutputFile=$gs_destination_filename " . $arguments_gs;
-      $arguments_gs = str_replace('%s','', $arguments_gs);
-      $arguments_gs = str_replace_first('%file','%s', $arguments_gs);
+      $arguments_gs = str_replace('%s', '', $arguments_gs);
+      $arguments_gs = str_replace_first('%file', '%s', $arguments_gs);
       $arguments_gs = sprintf($arguments_gs, $file_path);
 
-      $arguments_tesseract = str_replace('%s','', $arguments_tesseract);
-      $arguments_tesseract = str_replace_first('%file','%s', $arguments_tesseract);
+      $arguments_tesseract = str_replace('%s', '', $arguments_tesseract);
+      $arguments_tesseract = str_replace_first('%file', '%s', $arguments_tesseract);
       $arguments_tesseract = sprintf($arguments_tesseract, $gs_destination_filename);
 
-      $command_gs = escapeshellcmd($execpath_gs.' '.$arguments_gs);
-      $command_tesseract = escapeshellcmd($execpath_tesseract.' '.$arguments_tesseract);
+      $command_gs = escapeshellcmd($execpath_gs . ' ' . $arguments_gs);
+      $command_tesseract = escapeshellcmd($execpath_tesseract . ' ' . $arguments_tesseract);
 
-      $command = $command_gs.' && '.$command_tesseract;
+      $command = $command_gs . ' && ' . $command_tesseract;
 
-    } else {
+    }
+    else {
       error_log("missing arguments for OCR");
     }
     // Only return $command if it contains the original filepath somewhere
-    if (strpos($command, $file_path) !== false) { return $command;}
+    if (strpos($command, $file_path) !== FALSE) {
+      return $command;
+    }
     return '';
 
   }
@@ -309,42 +314,57 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
       error_log('Could not convert HOCR to MiniOCR, sources is not valid XML');
       return NULL;
     }
-    $w = new \XMLWriter();
-    $w->openMemory();
-    $w->startDocument('1.0','UTF-8');
-    $w->startElement("ocr");
+    $miniocr = new \XMLWriter();
+    $miniocr->openMemory();
+    $miniocr->startDocument('1.0', 'UTF-8');
+    $miniocr->startElement("ocr");
     foreach ($hocr->body->children() as $page) {
       $coos = explode(" ", substr($page['title'], 5));
+      // To avoid divisions by 0
+      $pwidth = (float) $coos[2] ? (float) $coos[2] : 1;
+      $pheight = (float) $coos[3] ? (float) $coos[3] : 1;
       if (count($coos)) {
-        $w->startElement("p");
-        $w->writeAttribute("id", $pageid);
-        $w->writeAttribute("wh", $coos[2] . " " . $coos[3]);
-        $w->startElement("b");
-        foreach ($page->children() as $line) {
-          $w->startElement("l");
+        $miniocr->startElement("p");
+        $miniocr->writeAttribute("xml:id", $pageid);
+        $miniocr->writeAttribute("wh", $pwidth . " " . $pheight);
+        $miniocr->startElement("b");
+        $page->registerXPathNamespace('ns', 'http://www.w3.org/1999/xhtml');
+        foreach ($page->xpath('.//ns:span[@class="ocr_line"]') as $line) {
+          $miniocr->startElement("l");
           foreach ($line->children() as $word) {
             $wcoos = explode(" ", $word['title']);
             if (count($wcoos)) {
-              $w->startElement("w");
-              $w->writeAttribute("x", $wcoos[1] . ' ' . $wcoos[2] . ' ' . $wcoos[3] . ' ' . $wcoos[4]);
-              error_log($word->__toString());
-              $w->text($word->__toString());
-              $w->endElement();
+              $x0 = (float) $wcoos[1];
+              $y0 = (float) $wcoos[2];
+              $x1 = (float) $wcoos[3];
+              $y1 = (float) $wcoos[4];
+              $l = round(($x0 / $pwidth), 3);
+              $t = round(($y0 / $pheight), 3);
+              $w = round((($x1 - $x0) / $pwidth), 3);
+              $h = round((($y1 - $y0) / $pheight), 3);
+              $text = (string) $word;
+              $miniocr->startElement("w");
+              $miniocr->writeAttribute("x", $l . ' ' . $t . ' ' . $w . ' ' . $h);
+
+              $miniocr->text($text);
+              $miniocr->endElement();
             }
           }
-          $w->endElement();
+          $miniocr->endElement();
         }
-        $w->endElement();
-        $w->endElement();
+        $miniocr->endElement();
+        $miniocr->endElement();
       }
     }
-    $w->endElement();
-    $w->endDocument();
+    $miniocr->endElement();
+    $miniocr->endDocument();
     unset($hocr);
-    return $w->outputMemory(true);
+
+    return $miniocr->outputMemory(TRUE);
   }
 
-
-
-
 }
+
+
+
+
