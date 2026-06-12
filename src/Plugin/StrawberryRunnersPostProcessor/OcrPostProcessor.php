987fe80d3704c091dcabd028e4ebb1e47c5d1c76
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2026-06-12 17:40:39 -0400

    Why are we returning NULL here without an else? So strange

--- src/Plugin/StrawberryRunnersPostProcessor/OcrPostProcessor.php
+++ src/Plugin/StrawberryRunnersPostProcessor/OcrPostProcessor.php
@@ -339,6 +339,7 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
       $sequence_number = isset($io->input->{$input_argument}) ? (int) $io->input->{$input_argument} : 1;
       setlocale(LC_CTYPE, 'en_US.UTF-8');
       $execstring_pdfalto = $this->buildExecutableCommand_pdfalto($io);
+
       if ($execstring_pdfalto) {
         $backup_locale = setlocale(LC_CTYPE, '0');
         setlocale(LC_CTYPE, $backup_locale);
@@ -782,7 +783,9 @@ class OcrPostProcessor extends SystemBinaryPostProcessor {
       //escapeshellcmd breaks 2>/dev/null
       $command = $command_pdfalto. " 2>/dev/null - ";
     }
-    return NULL;
+    else {
+      return NULL;
+    }
     // Only return $command if it contains the original filepath somewhere
     if (strpos($command, $file_path) !== FALSE) {
       return $command;
